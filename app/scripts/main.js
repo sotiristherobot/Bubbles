0b98e1aac7612f892e4476f0db2719ff14936211
Sotiris Ioannou <me@sotiris.engineer>
2016-09-24 22:37:12 -0400

    generateBubble() -- Generate and return a bubble, append to html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
+
+//generates a single bubble
+function generateBubble(){
+  const bubble = document.createElement("div");
+  bubble.className += "bubble";
+  return bubble;
+}
+
 //generate and return an array of bubbles
 function generateBubbles(){
   //generate a random number for bubbles
@@ -7,8 +15,12 @@ function generateBubbles(){
   const bubblesArray = [];
 
   for (let i = 0; i < numberOfBubbles; i++)
-    bubblesArray.push(createBubble());
+    bubblesArray.push(generateBubble());
 
+  //append bubbles to html
+  for (let i = 0; i < numberOfBubbles; i++){
+    document.getElementById("bubblesArea").appendChild(bubblesArray[i]);
+  }
   return bubblesArray;
 }
 
