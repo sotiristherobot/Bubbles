dd79c1cda48feece098f6fd11f5cc0c12c041b90
Sotiris Ioannou <me@sotiris.engineer>
2016-09-24 22:20:52 -0400

    generateBubbles() -- Generate and return an array of bubbles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+//generate and return an array of bubbles
+function generateBubbles(){
+  //generate a random number for bubbles
+  const numberOfBubbles = Math.floor((Math.random() * 10) + 1);
+
+  //bubbles array to hold the generated bubbles
+  const bubblesArray = [];
+
+  for (let i = 0; i < numberOfBubbles; i++)
+    bubblesArray.push(createBubble());
+
+  return bubblesArray;
+}
+
+generateBubbles();
